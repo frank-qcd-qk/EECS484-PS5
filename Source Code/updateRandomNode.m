b6b41f90e480a48d08ea2da7de217a6191de9081
frank-qcd-qk <cxq41@case.edu>
2018-11-07 22:01:32 -0500

    fixed minor issue

--- Source Code/updateRandomNode.m
+++ Source Code/updateRandomNode.m
@@ -1,7 +1,7 @@
 % fnc to correct nodes in matrix memory
 % ! Function completed
 function [vec, delta] = updateRandomNode(MatrixMem, vec)
-    indexing = random('unid', length(vec));
+    indexing = unidrnd(length(vec));
     difference = vec(indexing);
     vec(indexing) = sign(MatrixMem(indexing, :) * vec);
     delta = abs(difference - vec(indexing)) / 2;
